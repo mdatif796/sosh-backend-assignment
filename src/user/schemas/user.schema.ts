2f75b87948ace0ba9dfed874c4adc5ed2158deaf
mdatif796 <mdatif796@gmail.com>
2023-02-03 16:56:00 +0530

    created User schema

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
